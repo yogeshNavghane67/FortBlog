1f2df73c10f8e4ddb02abb69cfc40c899fa6e7fb
Yogesh Navghane <yogeshnavghane661@gmail.com>
2025-07-11 23:30:01 +0530

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Blog from './pages/Blog'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <div>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/blog' element={<Blog/>}/>
+        </Routes>
+      </div>
+    </div>
   )
 }
 
