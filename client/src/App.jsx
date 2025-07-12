d4aa8ac4a1b3eddd9fb8ef225da7ed32576e79a6
Yogesh Navghane <yogeshnavghane661@gmail.com>
2025-07-13 00:45:29 +0530

    BlogCards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
       <div>
         <Routes>
           <Route path='/' element={<Home/>}/>
-          <Route path='/blog' element={<Blog/>}/>
+          <Route path='/blog/:id' element={<Blog/>}/>
         </Routes>
       </div>
     </div>
--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
-import { blogCategories } from "../assets/assets"
+import { blog_data, blogCategories } from "../assets/assets"
+import BlogCard from "./BlogCard"
+
 
 const BlogList = () => {
     const [menu, setMenu] = useState("All")
@@ -10,14 +12,18 @@ const BlogList = () => {
                 <div key={item} className="relative">
                     <button  onClick={()=>setMenu(item)} className={`cursor-pointer text-gray-500 ${menu === item && 'text-white px-4 pt-0.5'}`}>
                         {item}
-                        {menu === item && ( <div className="absolute left-0  right-0 top-0 h-7 -z-1 bg-primary rounded-full"></div>)}
+                        {menu === item && ( 
+                            <div
+                            className="absolute left-0  right-0 top-0 h-7 -z-1 bg-primary rounded-full">
+                            </div>
+                        )}
                        
                     </button>
                 </div>
             ))}
         </div>
-        <div>
-            {/* blog cards */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40">
+            {blog_data.filter((blog)=> menu === "All" ? true : blog.category === menu).map((blog)=> <BlogCard key={blog._id} blog={blog}/>)}
         </div>
     </div>
   )
