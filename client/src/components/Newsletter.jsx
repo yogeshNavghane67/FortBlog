1deffdd44f0e44e8b44dd51b0c3478e7532717fc
Yogesh Navghane <yogeshnavghane661@gmail.com>
2025-07-15 23:44:11 +0530

    newsletter

---
+++ client/src/components/Newsletter.jsx
@@ -0,0 +1,15 @@
+
+const Newsletter = () => {
+  return (
+    <div className="flex flex-col items-center justify-center text-center space-y-2 my-32">
+    <h1 className="md:text-4xl text-2xl font-semibold">Never Miss a Blog!</h1>
+    <p className="md:text-lg text-gray-500/70 pb-8">Subscribe to get the latest blog, new tech, and exclusive news.</p>
+    <form className="flex items-center justify-between max-w-2xl w-full md:h-13 h-12">
+        <input className="border border-gray-300 rounded-md h-full border-r-0 outline-none w-full rounded-r-none px-3 text-gray-500" type="text" placeholder="Enter your email id" required />
+        <button type="submit" className="md:px-12 px-8 h-full text-white bg-primary/80 hover:bg-primary transition-all cursor-pointer rounded-md rounded-1-none">Subscribe</button>
+    </form>
+    </div>
+  )
+}
+
+export default Newsletter
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import BlogList from "../components/BlogList"
 import Header from "../components/Header"
 import Navbar from "../components/Navbar"
+import Newsletter from "../components/Newsletter"
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Navbar/>
       <Header/>
       <BlogList/>
+      <Newsletter/>
     </>
   )
 }
