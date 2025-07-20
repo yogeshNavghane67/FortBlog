1c17fb970773dbcb1f261796c21786e8172c7829
Yogesh Navghane <yogeshnavghane661@gmail.com>
2025-07-20 16:54:59 +0530

    commet section

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-import { assets, blog_data } from "../assets/assets"
+import { assets, blog_data, comments_data } from "../assets/assets"
 import Navbar from "../components/Navbar"
 import Moment from "moment"
 
@@ -8,14 +8,20 @@ const Blog = () => {
   const {id} = useParams()
 
   const [data, setData] = useState(null)
+  const [comments, setComments] = useState([])
 
   const fetchBlogData = async () => {
     const data = blog_data.find(item => item._id === id)
     setData(data)
   }
 
+  const fetchComments = async () => {
+    setComments(comments_data)
+  }
+
   useEffect(()=>{
     fetchBlogData()
+    fetchComments()
   },[])
 
   return data ? (
@@ -33,6 +39,24 @@ const Blog = () => {
       
       <div className="mx-5 max-w-5xl md:mx-auto my-10 mt-6">
         <img src={data.image} alt="" className="rounded-3xl mb-5" />
+        <div className="rich-text max-w-3xl mx-auto" dangerouslySetInnerHTML={{__html: data.description}}></div>
+
+        {/* comment section */}
+        <div className="mt-14 mb-10 max-w-3xl mx-auto">
+          <p className="font-semibold mb-4">Comments ({comments.length})</p>
+          <div className="flex flex-col gap-4">
+            {comments.map((item,index)=>(
+              <div key={index} className="relative bg-primary/2 border border-primary/5 max-w-xl p-4 rounded text-gray-600">
+                <div className="flex items-center gap-2 mb-2">
+                  <img src={assets.user_icon} alt="" className="w-6" />
+                  <p className="font-medium">{item.name}</p>
+                </div>
+                <p className="text-sm max-w-md ml-8">{item.content}</p>
+                <div className="absolute right-4 bottom-3 flex items-center gap-2 text-sm">{Moment(item.createdAt).fromNow()}</div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   ) : (
