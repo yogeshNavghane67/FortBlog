3447e359cc591f592220a62ac8e1d5d68edb4dd1
Yogesh Navghane <yogeshnavghane661@gmail.com>
2025-08-01 00:13:59 +0530

    add blog checkbox

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -58,6 +58,13 @@ const AddBlog = () => {
             return <option key={index} value={item}>{item}</option>
           })}
          </select>
+
+         <div className="flex gap-2 mt-4">
+          <p>Publish Now</p>
+          <input type="checkbox" checked={isPublished} className="scale-125 cursor-pointer" onChange={e => setIsPublished(e.target.checked)} />
+         </div>
+
+         <button type="submit" className="mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm">Add Blog</button>
       </div>
     </form>
   )
