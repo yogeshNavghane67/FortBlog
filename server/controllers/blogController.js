3a46e02e3eed708a5ef20935819b00c93e694e93
Yogesh Navghane <yogeshnavghane661@gmail.com>
2025-09-16 23:32:08 +0530

    get all blogbyId & delete

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -40,3 +40,50 @@ export const addBlog = async (req,res) =>{
         res.json({success: false, message: error.message})
     }
 }
+
+export const getAllBlogs = async (req, res) => {
+    try {
+        const blogs = await Blog.find({isPublished: true})
+        res.json({success: true, blogs})
+        
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
+
+export const getBlogById = async (req, res) => {
+    try {
+        const {blogId} = req.params;
+        const blog = await Blog.findById(blogId)
+        if(!blog){
+            return res.json({ success: false, message: "Blog bot found"})
+        }
+        res.json({success: true, blog})
+        
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
+
+export const deleteBlogById = async (req, res) => {
+    try {
+        const {id} = req.body;
+        await Blog.findByIdAndDelete(id);
+        res.json({success: true, message: "Blog deleted successfully"})
+        
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
+
+export const togglePublish = async (req, res) => {
+    try {
+        const {id} = req.body;
+        const blog = await Blog.findById(id);
+        blog.isPublished = !blog.isPublished;
+        await blog.save();
+        res.json({success: true, message: 'Blog status Updated'})
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { addBlog } from "../controllers/blogController.js";
+import { addBlog, deleteBlogById, getAllBlogs, getBlogById, togglePublish } from "../controllers/blogController.js";
 import upload from "../middleware/multer.js";
 import auth from "../middleware/auth.js";
 
 const blogRouter = express.Router();
 
 blogRouter.post("/add",upload.single('image'), auth, addBlog)
+blogRouter.get("/all", getAllBlogs);
+blogRouter.get("/:blogId", getBlogById);
+blogRouter.post("/delete", auth, deleteBlogById);
+blogRouter.post('/toggle-publish', auth, togglePublish);
 
 export default blogRouter;
+
+
